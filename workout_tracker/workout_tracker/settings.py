4078020336a01eabfc33dd50188d355b6c117d76
Giancarlos <enmadofenyxz@gmail.com>
2025-04-19 20:31:46 -0400

    Update settings.py

--- workout_tracker/workout_tracker/settings.py
+++ workout_tracker/workout_tracker/settings.py
@@ -148,7 +148,7 @@ STORAGES = {
    #     "BACKEND": "django.core.files.storage.FileSystemStorage",
    # },
    "staticfiles": {
-       "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+       "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
    },
 }
 
